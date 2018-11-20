597e1fc899c29613efb56e93ff992093b00fa8ff
yike lu <13263406073@163.com>
2018-11-20 10:36:33 +0800

    attitude roll real success with leso   and  rotate 360du  add qgc parameters

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ git status
 
 git add .
 
-git commit -m 'attitude roll real success with leso   and  rotate 360du '
+git commit -m 'attitude roll real success with leso   and  rotate 360du  add qgc parameters '
 
 git push
 
